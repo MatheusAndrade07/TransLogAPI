3af3e8d0bb42bec1d2f0e7685350ebbbfe2b424a
Matheus <matheusandradepompermayer07@gmail.com>
2024-03-27 21:53:42 -0300

    Reorganização dos projetos.

---
+++ FrontEnd/Model/Response/CargaResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontEnd.Models.Response
+{
+    public class CargaResponse
+    {
+        public decimal Distancia { get; set; }
+        public decimal Peso { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Largura { get; set; }
+        public decimal Comprimento { get; set; }
+    }
+
+}
